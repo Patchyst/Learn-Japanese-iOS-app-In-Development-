a195ba69fefa3d09209185d65dc48679360c6892
Patrick Story <ptedstoryv2@gmail.com>
2020-06-05 13:14:07 -0400

    Update QuestionGroup.swift

--- LearnJapanese/Main/QuestionGroup.swift
+++ LearnJapanese/Main/QuestionGroup.swift
@@ -9,13 +9,5 @@
 import SwiftUI
 
 struct QuestionGroup: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-struct QuestionGroup_Previews: PreviewProvider {
-    static var previews: some View {
-        QuestionGroup()
-    }
 }
