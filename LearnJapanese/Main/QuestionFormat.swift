417237d5ea9b1079e97a036c60b7286d0fedc658
Patrick Story <ptedstoryv2@gmail.com>
2020-06-04 23:30:12 -0400

    Update QuestionFormat.swift

--- LearnJapanese/Main/QuestionFormat.swift
+++ LearnJapanese/Main/QuestionFormat.swift
@@ -1,13 +1,8 @@
 import SwiftUI
 
 struct QuestionFormat{
-    let question: String
-    let incorrectAnswers: [String]
-    let answer: String
-    init(question: String, answer: String, incorrectAnswers: [String]) {
-        self.question = question
-        self.answer = answer
-        self.incorrectAnswers = incorrectAnswers
+    func checkMatch(valueOne: Int, valueTwo: Int, endRange: Int) -> Int{
+
     }
 }
 
