1cbdcb49895cc19eb4d1427b34de2ec5d2ef5c5c
Patrick Story <ptedstoryv2@gmail.com>
2020-06-05 17:05:29 -0400

    Update HiraganaQuizViewController.swift

--- LearnJapanese/Controller/HiraganaQuizViewController.swift
+++ LearnJapanese/Controller/HiraganaQuizViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class HiraganaQuizViewController: UIViewController {
     var hiraganaLogic = HiraganaLogic()
+    var questionFormat = QuestionFormat()
     var quizList = [[""]]
     @IBOutlet weak var answerOne: UIButton!
     @IBOutlet weak var answerTwo: UIButton!
     @IBOutlet weak var answerThree: UIButton!
+    @IBOutlet weak var questionText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let quizGroup = hiraganaLogic.generateQuestions(questionLength: quizList.count, questionGroup: quizList)
+        let quizQuestions = QuestionGroup(question: quizGroup[0], answers: [quizGroup[1], quizGroup[2], quizGroup[-1]])
+        let question = hiraganaLogic.answerDict[quizQuestions.question]
+
     }
-    
     @IBAction func answerButton(_ sender: UIButton) {
-        let questionGroup = hiraganaLogic.generateQuestions(questionLength: quizList.count, questionGroup: quizList)
-        // Unfin
+
+        
     }
-}   
 }
