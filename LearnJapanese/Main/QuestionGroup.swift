7755b8c2164fecd08f3269c739702511e8c596e4
Patrick Story <ptedstoryv2@gmail.com>
2020-06-05 13:13:36 -0400

    Add files via upload

---
+++ LearnJapanese/Main/QuestionGroup.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionGroup.swift
+//  LearnJapanese
+//
+//  Created by Patrick Story on 6/5/20.
+//  Copyright © 2020 Patrick Story. All rights reserved.
+//
+
+import SwiftUI
+
+struct QuestionGroup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuestionGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionGroup()
+    }
+}
