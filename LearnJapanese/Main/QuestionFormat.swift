1b603ed179884363993269c273b6c69540d55c0d
Patrick Story <ptedstoryv2@gmail.com>
2020-06-05 12:14:21 -0400

    Update QuestionFormat.swift

--- LearnJapanese/Main/QuestionFormat.swift
+++ LearnJapanese/Main/QuestionFormat.swift
@@ -4,16 +4,20 @@ struct QuestionFormat{
     func checkMatch(valueOne: Int, valueTwo: Int!, endRange: Int) -> Int{
         var questionIndex: Int = 0
         for value in 0 ... endRange{
-            switch value {
-            case _ where value != valueOne:
-             questionIndex = value
-                break
-             case _ where value == valueOne:
-                continue
-             default:
-                questionIndex = 0
+            if valuetwo == nil{
+                switch value {
+                case _ where value != valueOne:
+                    questionIndex = value
+                    break
+                case _ where value == valueOne:
+                    continue
+                default:
+                    questionIndex = 0
                 }
-        }
+            }
+            else if valuetwo != nil{
+                
+            }
     }
 }
 
