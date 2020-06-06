31531909dc9bf6df7d5bb31cfd0ae1b74c17ba5e
Patrick Story <ptedstoryv2@gmail.com>
2020-06-06 16:26:08 -0400

    Update HiraganaQuizViewController.swift

--- LearnJapanese/Controller/HiraganaQuizViewController.swift
+++ LearnJapanese/Controller/HiraganaQuizViewController.swift
@@ -16,14 +16,24 @@ class HiraganaQuizViewController: UIViewController {
     @IBOutlet weak var answerTwo: UIButton!
     @IBOutlet weak var answerThree: UIButton!
     @IBOutlet weak var questionText: UILabel!
+    func questionGen(){
+        let quizGroup = hiraganaLogic.generateQuestions(questionLength: quizList.count, questionGroup: quizList)
+        print(quizGroup)
+        let quizQuestions = QuestionGroup(question: quizGroup[0], answers: [quizGroup[0], quizGroup[1], quizGroup[2]])
+        let question = hiraganaLogic.answerDict[quizQuestions.question]
+        print(quizQuestions.answers)
+        questionText.text = question ?? "No Q"
+        answerOne.setTitle(quizQuestions.answers[0], for: .normal)
+        answerTwo.setTitle(quizQuestions.answers[1], for: .normal)
+        answerThree.setTitle(quizQuestions.answers[2], for: .normal)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        let quizGroup = hiraganaLogic.generateQuestions(questionLength: quizList.count, questionGroup: quizList)
-        let quizQuestions = QuestionGroup(question: quizGroup[0], answers: [quizGroup[1], quizGroup[2], quizGroup[-1]])
-        let question = hiraganaLogic.answerDict[quizQuestions.question]
-
+        questionGen()
     }
     @IBAction func answerButton(_ sender: UIButton) {
+        questionGen()
+
 
         
     }
