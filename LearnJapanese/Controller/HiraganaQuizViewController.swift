1ffe274cdfe67a01089d9706700c2e97d2ce93fc
Patrick Story <ptedstoryv2@gmail.com>
2020-06-14 14:41:44 -0400

    fixing previous question wrong problem

--- LearnJapanese/Controller/HiraganaQuizViewController.swift
+++ LearnJapanese/Controller/HiraganaQuizViewController.swift
@@ -9,32 +9,51 @@
 import UIKit
 
 class HiraganaQuizViewController: UIViewController {
+    
     var hiraganaLogic = HiraganaLogic()
     var questionFormat = QuestionFormat()
+    
     var quizList = [[""]]
     @IBOutlet weak var answerOne: UIButton!
     @IBOutlet weak var answerTwo: UIButton!
     @IBOutlet weak var answerThree: UIButton!
     @IBOutlet weak var questionText: UILabel!
-    func questionGen(){
-        let quizGroup = hiraganaLogic.generateQuestions(questionLength: quizList.count, questionGroup: quizList)
-        print(quizGroup)
-        let quizQuestions = QuestionGroup(question: quizGroup[0], answers: [quizGroup[0], quizGroup[1], quizGroup[2]])
-        let question = hiraganaLogic.answerDict[quizQuestions.question]
-        print(quizQuestions.answers)
-        questionText.text = question ?? "No Q"
-        answerOne.setTitle(quizQuestions.answers[0], for: .normal)
-        answerTwo.setTitle(quizQuestions.answers[1], for: .normal)
-        answerThree.setTitle(quizQuestions.answers[2], for: .normal)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         questionGen()
     }
     @IBAction func answerButton(_ sender: UIButton) {
-        questionGen()
-
-
+        // The output of questionGen is the correct answer.
         
+        let senderTitle = sender.titleLabel?.text ?? "na"
+        let correctAnswer = questionText.text ?? "na"
+        let userAnswer = hiraganaLogic.answerDict[senderTitle]
+        
+        if userAnswer == correctAnswer{
+            print("true")
+        }
+        else{
+            print("false")
+        }
+        // generate a new question
+        questionGen()
+    }
+    
+    func questionGen(){
+        let quizGroup = hiraganaLogic.generateQuestions(questionLength: quizList.count, questionGroup: quizList)
+        // the first element(0) is the question, the answers list includes 2 wrong answers and one matching element(0)
+        let quizQuestions = QuestionGroup(question: quizGroup[0], answers: [quizGroup[0], quizGroup[1], quizGroup[2]])
+        // converting element the Hiragana question element to English using the answer dictionary
+        let question = hiraganaLogic.answerDict[quizQuestions.question] ?? "ERROR: No Question supplied"
+        // randomizing quesiton location so correct answer doesn't show up on the same button every time. Explanation for how the checkMatch function can be found in QuestionFormat.swift
+        let indexRange = quizQuestions.answers.count - 1
+        let randomQuestionIndex = Int.random(in: 0 ... indexRange)
+        let questionOneIndex = questionFormat.checkMatch(valueOne: randomQuestionIndex, valuetwo: nil, endRange: indexRange)
+        let questionTwoIndex = questionFormat.checkMatch(valueOne: randomQuestionIndex, valuetwo: questionOneIndex, endRange: indexRange)
+        // setting button titles
+        answerOne.setTitle(quizQuestions.answers[randomQuestionIndex], for: .normal)
+        answerTwo.setTitle(quizQuestions.answers[questionOneIndex], for: .normal)
+        answerThree.setTitle(quizQuestions.answers[questionTwoIndex], for: .normal)
+        questionText.text = question
     }
 }
