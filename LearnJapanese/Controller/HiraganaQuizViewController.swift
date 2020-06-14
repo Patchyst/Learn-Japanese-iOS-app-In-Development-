c418cd4e8310a10a72469f124a6c5a4f952467a2
Patrick Story <ptedstoryv2@gmail.com>
2020-06-14 15:34:08 -0400

    Update HiraganaQuizViewController.swift

--- LearnJapanese/Controller/HiraganaQuizViewController.swift
+++ LearnJapanese/Controller/HiraganaQuizViewController.swift
@@ -14,6 +14,7 @@ class HiraganaQuizViewController: UIViewController {
     var questionFormat = QuestionFormat()
     
     var quizList = [[""]]
+    
     @IBOutlet weak var answerOne: UIButton!
     @IBOutlet weak var answerTwo: UIButton!
     @IBOutlet weak var answerThree: UIButton!
@@ -30,30 +31,39 @@ class HiraganaQuizViewController: UIViewController {
         let userAnswer = hiraganaLogic.answerDict[senderTitle]
         
         if userAnswer == correctAnswer{
-            print("true")
+            sender.backgroundColor = UIColor.green
         }
         else{
-            print("false")
+            sender.backgroundColor = UIColor.red
         }
-        // generate a new question
-        questionGen()
+        // delay 0.2 seconds so user can see if their answer was correct or not
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(questionGen), userInfo: nil, repeats: false)
     }
     
-    func questionGen(){
+    @objc func questionGen(){
+        
         let quizGroup = hiraganaLogic.generateQuestions(questionLength: quizList.count, questionGroup: quizList)
         // the first element(0) is the question, the answers list includes 2 wrong answers and one matching element(0)
         let quizQuestions = QuestionGroup(question: quizGroup[0], answers: [quizGroup[0], quizGroup[1], quizGroup[2]])
+        
         // converting element the Hiragana question element to English using the answer dictionary
         let question = hiraganaLogic.answerDict[quizQuestions.question] ?? "ERROR: No Question supplied"
+        
         // randomizing quesiton location so correct answer doesn't show up on the same button every time. Explanation for how the checkMatch function can be found in QuestionFormat.swift
         let indexRange = quizQuestions.answers.count - 1
         let randomQuestionIndex = Int.random(in: 0 ... indexRange)
         let questionOneIndex = questionFormat.checkMatch(valueOne: randomQuestionIndex, valuetwo: nil, endRange: indexRange)
         let questionTwoIndex = questionFormat.checkMatch(valueOne: randomQuestionIndex, valuetwo: questionOneIndex, endRange: indexRange)
+        
         // setting button titles
         answerOne.setTitle(quizQuestions.answers[randomQuestionIndex], for: .normal)
         answerTwo.setTitle(quizQuestions.answers[questionOneIndex], for: .normal)
         answerThree.setTitle(quizQuestions.answers[questionTwoIndex], for: .normal)
         questionText.text = question
+        // setting all Question background colors back to white
+        answerOne.backgroundColor = UIColor.white
+        answerTwo.backgroundColor = UIColor.white
+        answerThree.backgroundColor = UIColor.white
+        
     }
 }
